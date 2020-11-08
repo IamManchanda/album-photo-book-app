51c7e8b93b5d5f7d82e377cfc588b2c929242b9b
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-11-08 16:09:52 +0530

    Implement Tailwind

--- src/components/hello-world.vue
+++ src/components/hello-world.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="component-hello-world">
-    <h1>{{ msg }}</h1>
+    <h1 class="text-xl">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./app.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "./assets/styles/scss/tailwind.scss";
 
 createApp(App)
   .use(store)
