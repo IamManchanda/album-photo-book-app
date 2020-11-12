15729e17fd5cff043181679464951ad28d0a448b
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-11-12 13:53:24 +0530

    State Management: Auth

---
+++ src/store/auth/auth.js
@@ -0,0 +1,66 @@
+import { Auth } from "aws-amplify";
+
+export const auth = {
+  namespace: true,
+  state: {
+    user: null,
+  },
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
+  },
+  actions: {
+    logout: async ({ commit }, _payload) => {
+      commit("setUser", null);
+      return await Auth.signOut();
+    },
+    login: async ({ commit }, { username, password }) => {
+      try {
+        await Auth.signIn({
+          username,
+          password,
+        });
+
+        const userInfo = await Auth.currentUserInfo();
+        commit("setUser", userInfo);
+
+        return Promise.resolve("Success");
+      } catch (error) {
+        console.log(error);
+        return Promise.reject(error);
+      }
+    },
+    confirmSignup: async (_context, { username, code }) => {
+      try {
+        await Auth.confirmSignUp(username, code);
+        return Promise.resolve();
+      } catch (error) {
+        console.log(error);
+        return Promise.reject(error);
+      }
+    },
+    signup: async (_context, { username, password, email }) => {
+      try {
+        await Auth.signUp({
+          username,
+          password,
+          attributes: {
+            email,
+          },
+        });
+        return Promise.resolve();
+      } catch (error) {
+        console.log(error);
+        return Promise.reject(error);
+      }
+    },
+    authAction: async ({ commit }, _payload) => {
+      const userInfo = await Auth.currentUserInfo();
+      commit("setUser", userInfo);
+    },
+  },
+  getters: {
+    user: state => state.user,
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
 import { createStore } from "vuex";
+import { auth } from "./auth/auth";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: { auth },
 });
